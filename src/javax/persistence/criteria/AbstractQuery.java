6df082ed572a4d32e83bcd57954e698cd722870a
Eric Gwin <eric.gwin@oracle.com>
2012-02-10 15:57:10 -0500

    Initial 2.1 API codebase from Linda DeMichiel Build changes for 2.1.0.v201201192338

--- src/javax/persistence/criteria/AbstractQuery.java
+++ src/javax/persistence/criteria/AbstractQuery.java
@@ -1,24 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Sun Microsystems. All rights reserved. 
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
- * http://www.eclipse.org/org/documents/edl-v10.php.
- * 
- * Contributors:
- *     Linda DeMichiel - Java Persistence 2.0 - Version 2.0 (October 1, 2009)
- *     Specification available from http://jcp.org/en/jsr/detail?id=317
+ * Copyright (c) 2008 - 2012 Oracle Corporation. All rights reserved.
  *
- ******************************************************************************/
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Linda DeMichiel - Java Persistence 2.1
+ *     Linda DeMichiel - Java Persistence 2.0
+ *
+ ******************************************************************************/ 
 package javax.persistence.criteria;
 
 import java.util.List;
 import java.util.Set;
-
-import javax.persistence.metamodel.EntityType;
 
 /**
  * The <code>AbstractQuery</code> interface defines functionality that is common
@@ -34,24 +32,7 @@ import javax.persistence.metamodel.EntityType;
  *
  * @since Java Persistence 2.0
  */
-public interface AbstractQuery<T> {
-
-    /**
-     * Create and add a query root corresponding to the given entity,
-     * forming a cartesian product with any existing roots.
-     * @param entityClass  the entity class
-     * @return query root corresponding to the given entity
-     */
-    <X> Root<X> from(Class<X> entityClass);
-
-    /**
-     * Create and add a query root corresponding to the given entity,
-     * forming a cartesian product with any existing roots.
-     * @param entity  metamodel entity representing the entity
-     *                of type X
-     * @return query root corresponding to the given entity
-     */
-    <X> Root<X> from(EntityType<X> entity);
+public interface AbstractQuery<T> extends CommonAbstractQuery {
 
     /**
      * Modify the query to restrict the query results according
@@ -129,13 +110,6 @@ public interface AbstractQuery<T> {
     AbstractQuery<T> distinct(boolean distinct);
 
     /**
-     * Create a subquery of the query. 
-     * @param type  the subquery result type
-     * @return subquery 
-     */
-    <U> Subquery<U> subquery(Class<U> type);
-
-    /**
      * Return the query roots.  These are the roots that have
      * been defined for the <code>CriteriaQuery</code> or <code>Subquery</code> itself,
      * including any subquery roots defined as a result of
@@ -152,14 +126,6 @@ public interface AbstractQuery<T> {
      */
     Selection<T> getSelection();
 
-    /**
-     * Return the predicate that corresponds to the where clause
-     * restriction(s), or null if no restrictions have been
-     * specified.
-     * @return where clause predicate
-     */
-    Predicate getRestriction();
- 
     /**
      * Return a list of the grouping expressions.  Returns empty
      * list if no grouping expressions have been specified.
