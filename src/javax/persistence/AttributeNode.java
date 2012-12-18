b68ec2b905d93e1d01f932dd75b34b08a1a3e472
gpelletie <guy.pelletier@oracle.com>
2012-12-18 16:33:15 -0500

    Bug 338610 - JPA 2.1 Functionality for Java EE 7 (JSR-338)
    New JPA 2.1 classes.

---
+++ src/javax/persistence/AttributeNode.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2008 - 2012 Oracle Corporation. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Linda DeMichiel - Java Persistence 2.1
+ *
+ ******************************************************************************/
+
+package javax.persistence;
+
+import javax.persistence.metamodel.Type;
+
+/**
+ * Represents an attribute node of an entity graph.
+ *
+ * @param <T> The type of the attribute.
+ *
+ * @see EntityGraph
+ *
+ * @since Java Persistence 2.1
+ */
+public interface AttributeNode<T> {
+
+    /**
+     * Return the Type of the attribute node.
+     * @return Type object for the attribute node
+     */
+    public Type<T> getType();
+
+    /**
+     * Return the name of the attribute corresponding to the
+     * attribute node.
+     * @return name of the attribute
+     */
+    public String getAttributeName();
+}
+
