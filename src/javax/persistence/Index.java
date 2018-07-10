35bbaad8d986403deea026a7459fd0375aa31495
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-07-10 13:37:26 +0200

    update license to EPL20
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- src/javax/persistence/Index.java
+++ src/javax/persistence/Index.java
@@ -1,17 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2011 - 2013 Oracle Corporation. All rights reserved.
+/*
+ * Copyright (c) 2011, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
- * which accompanies this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
- * Contributors:
- *     Linda DeMichiel - Java Persistence 2.1
- *
- ******************************************************************************/
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+
+// Contributors:
+//     Linda DeMichiel - Java Persistence 2.1
+
 package javax.persistence;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
