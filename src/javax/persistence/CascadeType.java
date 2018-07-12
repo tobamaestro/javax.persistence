82dd749e34c0d7d9d05b3104dd55241198378418
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-07-12 18:05:05 +0200

    fix comp error
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- src/javax/persistence/CascadeType.java
+++ src/javax/persistence/CascadeType.java
@@ -16,15 +16,6 @@
 
 package javax.persistence;
 
-/////
-// Defines the set of cascadable operations that are propagated
-// to the associated entity.
-// The value <code>cascade=ALL</code> is equivalent to
-// <code>cascade={PERSIST, MERGE, REMOVE, REFRESH, DETACH}</code>.
-
-// @since Java Persistence 1.0
- package javax.persistence;
-
 /**
  * Defines the set of cascadable operations that are propagated 
  * to the associated entity.
